2d91b3c7a9be7f70a393b29941ef9f0243bdb6ca
rekky1aws <killikomfr@gmail.com>
2024-07-08 01:26:33 +0200

    Added use of pinia in main.js

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import './assets/styles.css'
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app')
