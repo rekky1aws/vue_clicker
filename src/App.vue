ed1cd741e77babf37a0e6e7f4c2bf33e4d38f773
rekky1aws <killikomfr@gmail.com>
2024-07-10 18:39:44 +0200

    Moved script of App.vue on top of the file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
+<script setup>
+import PartyArea from './components/PartyArea.vue'
+import FactoryList from './components/FactoryList.vue'
+</script>
+
 <template>
   <main class="app">
     <PartyArea />
     <FactoryList />
   </main>
 </template>
-
-<script setup>
-import PartyArea from './components/PartyArea.vue'
-import FactoryList from './components/FactoryList.vue'
-</script>
